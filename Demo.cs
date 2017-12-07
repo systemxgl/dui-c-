d7fb8ce69f9dd74e912f2f1e378317e0c1c2f86a
systemxgl <123218497@qq.com>
2017-12-07 15:40:58 +0800

    新增打印网页信息示例
    新增打印网页信息示例

--- Demo.cs
+++ Demo.cs
@@ -29,6 +29,8 @@ namespace ConsoleApplication1
             //格式详见 https://github.com/systemxgl/dui-api 或 http://www.mstching.com/openapi.pdf
             string jsonContent = "[{\"Alignment\":0,\"BaseText\":\"" + Utils.StringToBase64(content) + "\",\"Bold\":0,\"FontSize\":0,\"PrintType\":0}]";
             PrintHelper.PrintContent("您的设备编号", jsonContent, 0);//0 改成用户设备绑定返回的OpenUserId即可
+            string printUrl = "您要打印的网页地址";//例：http://www.open.mstching.com/print-demo.html
+            PrintHelper.PrintHtmlContent("您的设备编号",printUrl,0);//0 改成用户设备绑定返回的OpenUserId即可
             //查询任务状态
             PrintHelper.GetPrintTaskState(0);//0 改成任务编号即可 
         }
